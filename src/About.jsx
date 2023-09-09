af30fc94d41f70b9ae521c786adc767a1cf433dc
Midhun Muraleedharan <73743937+Midhun-7@users.noreply.github.com>
2023-09-09 23:37:20 +0530

    Update About page

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,9 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>
-        Write a brief introduction about yourself here. You can include your background, skills, and interests.
+      My name is Midhun Muraleedharan, and I am an aspiring software developer with a deep passion for machine learning. I am currently pursuing a degree in Computer Science and Engineering, where I have gained a solid foundation in various programming languages and technologies, including C++, Python, HTML5, CSS3, JavaScript, and React.js. </p>
+
+      <p>My interest in web development and machine learning led me to participate in several hackathons, where I have honed my skills in problem-solving, teamwork, leadership, and communication. In addition to my technical skills, I possess a strong work ethic, eagerness to learn, and a creative mindset, which allows me to approach complex problems with innovative solutions.
       </p>
     </div>
   );
